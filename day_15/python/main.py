5a7f1d5b0608b54122386b16c29e3eaedef44753
Dakota <dakota742009@gmail.com>
2024-12-18 16:58:43 +0100

    chore: MapUtils D15

--- day_15/python/main.py
+++ day_15/python/main.py
@@ -1,9 +1,9 @@
 from copy import deepcopy
 from functools import cache
 from typing import Tuple
-from utils.python import get_input_file_from_script_file
+from utils.python import get_input_file_from_script_file, MapUtils
 
-class Map:
+class Map(MapUtils):
     __slots__ = ['map', 'directions', 'robot_position', 'walls']
 
     def __init__(self, data: str):
@@ -14,65 +14,44 @@ class Map:
         self.robot_position: Tuple[int, int] = self.get_robot_position()
         self.walls: Tuple[Tuple[int, int]] = self.get_walls()
 
-    def get_next_position(self, position: Tuple[int, int], direction: str):
-        x, y = position
-
-        match direction:
-            case '^':
-                return x, y - 1
-            case '>':
-                return x + 1, y
-            case 'v':
-                return x, y + 1
-            case '<':
-                return x - 1, y
-
-    @cache
-    def is_position_out_of_bounds(self, position: Tuple[int, int]):
-        x, y = position
-        return y < 0 or y >= len(self.map) or x < 0 or x >= len(self.map[y])
+    @property
+    def ROBOT_SYMBOL(self):
+        return '@'
+    
+    @property
+    def WALL_SYMBOL(self):
+        return '#'
+    
+    @property
+    def BOX_SYMBOL(self):
+        return 'O'
+    
+    @property
+    def EMPTY_SYMBOL(self):
+        return '.'
     
     @cache
     def is_position_wall(self, position: Tuple[int, int]):
         return position in self.walls
-    
-    def get_position_value(self, position: Tuple[int, int]):
-        x, y = position
-        return self.map[y][x]
 
     def get_robot_position(self) -> Tuple[int, int]:
-        for y, row in enumerate(self.map):
-            for x, cell in enumerate(row):
-                if cell == '@':
-                    return (x, y)
+        return self.search_value(self.ROBOT_SYMBOL)[0]
                 
     def get_walls(self) -> Tuple[Tuple[int, int]]:
-        walls = []
-        for y, row in enumerate(self.map):
-            for x, cell in enumerate(row):
-                if cell == '#':
-                    walls.append((x, y))
-
-        return tuple(walls)
+        return self.search_value(self.WALL_SYMBOL)
     
     def get_boxes(self) -> Tuple[Tuple[int, int]]:
-        boxes = []
-        for y, row in enumerate(self.map):
-            for x, cell in enumerate(row):
-                if cell == 'O':
-                    boxes.append((x, y))
-
-        return tuple(boxes)
+        return self.search_value(self.BOX_SYMBOL)
     
     def how_much_distance_before_walls(self, position: Tuple[int, int], direction: str):
         distance = 1
-        next_pos = self.get_next_position(position, direction)
+        next_pos = self.get_position_to_direction(position, direction)
 
         while True:
-            if self.get_position_value(next_pos) == '#':
+            if self.get_cell_value_from_position(next_pos) == self.WALL_SYMBOL:
                 break
 
-            next_pos = self.get_next_position(next_pos, direction)
+            next_pos = self.get_position_to_direction(next_pos, direction)
             distance += 1
 
         return distance
@@ -86,41 +65,40 @@ class Map:
         return 100 * from_top + from_left
     
     def set_robot_position(self, position: Tuple[int, int]):
-        x, y = position
-        self.map[self.robot_position[1]][self.robot_position[0]] = '.'
-        self.map[y][x] = '@'
+        self.map[self.robot_position[1]][self.robot_position[0]] = self.EMPTY_SYMBOL
+        self.map[position[1]][position[0]] = self.ROBOT_SYMBOL
         self.robot_position = position
 
     def robot_attempt_to_push(self, box_position: Tuple[int, int], direction: str):
-        next_pos = self.get_next_position(box_position, direction)
-        next_pos_value = self.get_position_value(next_pos)
+        next_pos = self.get_position_to_direction(box_position, direction)
+        next_pos_value = self.get_cell_value_from_position(next_pos)
 
         while True:
             match next_pos_value:
-                case '#':
+                case self.WALL_SYMBOL:
                     break
-                case '.':
+                case self.EMPTY_SYMBOL:
                     robot_position = self.robot_position
-                    self.map[robot_position[1]][robot_position[0]] = '.'
+                    self.map[robot_position[1]][robot_position[0]] = self.EMPTY_SYMBOL
 
                     self.set_robot_position(box_position)
-                    self.map[next_pos[1]][next_pos[0]] = 'O'
+                    self.map[next_pos[1]][next_pos[0]] = self.BOX_SYMBOL
                     break
-                case 'O':
-                    next_pos = self.get_next_position(next_pos, direction)
-                    next_pos_value = self.get_position_value(next_pos)
+                case self.BOX_SYMBOL:
+                    next_pos = self.get_position_to_direction(next_pos, direction)
+                    next_pos_value = self.get_cell_value_from_position(next_pos)
                     continue
 
     def move_robot_in_direction(self, direction: str):
-        next_pos = self.get_next_position(self.robot_position, direction)
-        next_pos_value = self.get_position_value(next_pos)
+        next_pos = self.get_position_to_direction(self.robot_position, direction)
+        next_pos_value = self.get_cell_value_from_position(next_pos)
 
         match next_pos_value:
-            case '#':
+            case self.WALL_SYMBOL:
                 pass
-            case '.':
+            case self.EMPTY_SYMBOL:
                 self.set_robot_position(next_pos)
-            case 'O':
+            case self.BOX_SYMBOL:
                 self.robot_attempt_to_push(next_pos, direction)
 
     def run(self):

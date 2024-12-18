a7548cbd41d48167736b4222a4f0d2d41de616b2
Dakota <dakota742009@gmail.com>
2024-12-18 15:40:32 +0100

    chore: MapUtils D6

--- day_6/python/main.py
+++ day_6/python/main.py
@@ -1,34 +1,17 @@
 from typing import List, Literal, Tuple
-from utils.python import get_file
+from utils.python import get_file, MapUtils
 
-class Map:
+class Map(MapUtils):
     __guard_icon: Literal['^', '>', 'v', '<']
-    __guard_possible_icons = ['^', '>', 'v', '<']
     __visited_cells: List[Tuple[int, int]] = []
 
     def __init__(self, data: str):
-        self.__map = [x for x in data.split('\n')]
-        self.__guard_position = self.__get_guard_initial_position()
+        super().__init__(data, start_value='^')
+        self.__guard_position = self.start
+        self.__guard_icon = '^'
 
-    def __get_guard_initial_position(self):
-        for y, row in enumerate(self.__map):
-            for x, cell in enumerate(row):
-                if cell in self.__guard_possible_icons:
-                    self.__guard_icon = cell
-                    return x, y
-
-    def __get_next_position(self):
-        x, y = self.__guard_position
-
-        match self.__guard_icon:
-            case '^':
-                return x, y - 1
-            case '>':
-                return x + 1, y
-            case 'v':
-                return x, y + 1
-            case '<':
-                return x - 1, y
+    def __get_next_guard_position(self) -> Tuple[int, int]:
+        return self.get_position_to_direction(self.__guard_position, self.__guard_icon)
             
     def __turn_guard_right(self):
         match self.__guard_icon:
@@ -41,22 +24,18 @@ class Map:
             case '<':
                 self.__guard_icon = '^'
 
-    def __is_position_out_of_bounds(self, x, y):
-        return y < 0 or y >= len(self.__map) or x < 0 or x >= len(self.__map[y])
-
     def __is_guard_out_of_bounds(self):
-        x, y = self.__guard_position
-        return self.__is_position_out_of_bounds(x, y)
+        return self.is_position_out_of_bounds(self.__guard_position)
 
     def solve_part_one(self):
         while not self.__is_guard_out_of_bounds():
             self.__visited_cells.append(self.__guard_position)
-            next_pos_x, next_pos_y = self.__get_next_position()
-            if not self.__is_position_out_of_bounds(next_pos_x, next_pos_y) and self.__map[next_pos_y][next_pos_x] == '#':
+            next_guard_pos = self.__get_next_guard_position()
+            if not self.is_position_out_of_bounds(next_guard_pos) and self.get_cell_value_from_position(next_guard_pos) == '#':
                 self.__turn_guard_right()
-                next_pos_x, next_pos_y = self.__get_next_position()
+                next_guard_pos = self.get_position_to_direction(self.__guard_position, self.__guard_icon)
 
-            self.__guard_position = next_pos_x, next_pos_y
+            self.__guard_position = next_guard_pos
 
         return len(set(self.__visited_cells))
 
--- utils/python/__init__.py
+++ utils/python/__init__.py
@@ -1,2 +1,3 @@
 from .input import get_file
 from .decorators import deep_copy_arg
+from .map import MapUtils, DIRECTION_TYPE, CornersPositions, NSEWPositions, AroundPositions
